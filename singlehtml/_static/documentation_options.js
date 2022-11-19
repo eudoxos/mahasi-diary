f15aaf67753b0eabfe39423cf6742564d0cf4c9d
eudoxos <github@doxos.eu>
2022-11-19 21:07:45 +0000

    Deploying to gh-pages from @ eudoxos/mahasi-diary@b0589d4f239abc5726402091f25e08ecdfa10f65 🚀

--- singlehtml/_static/documentation_options.js
+++ singlehtml/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '78723e0',
+    VERSION: 'b0589d4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',
