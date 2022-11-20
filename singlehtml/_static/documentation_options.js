7f41b0812a8216adb71484ecc05279e3847b514c
eudoxos <github@doxos.eu>
2022-11-20 15:30:31 +0000

    Deploying to gh-pages from @ eudoxos/mahasi-diary@5d108c1c2386dcde306ff10d987a74e52c8c047f 🚀

--- singlehtml/_static/documentation_options.js
+++ singlehtml/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'b0589d4',
+    VERSION: '5d108c1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',
