b34910eeafaed701ea39992ac4865b1c1f8abd8e
eudoxos <github@doxos.eu>
2022-11-23 09:16:27 +0000

    Deploying to gh-pages from @ eudoxos/mahasi-diary@5ed9a1249e80241c89aaaf6f3fab8a4a45b32d82 🚀

--- epub/_static/documentation_options.js
+++ epub/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'e1066c2',
+    VERSION: '5ed9a12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'epub',
